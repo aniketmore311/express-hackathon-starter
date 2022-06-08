11abd9aa346a1c96bd51d69ea6a643eaa566ee9f
aniket <aniketavinashmore343@gmail.com>
2022-06-08 06:38:53 +0530

    add types to catchasync

--- src/utils/catchAsync.js
+++ src/utils/catchAsync.js
@@ -1,4 +1,11 @@
 //@ts-check
+/**
+ * @typedef {(req: import("express").Request, res: import("express").Response, next: import("express").NextFunction)=> Promise<any>} AsyncHandler
+ */
+/**
+ * @param {AsyncHandler} asyncHandler
+ * @returns {import("express").RequestHandler}
+ */
 module.exports = function (asyncHandler) {
   return function (req, res, next) {
     asyncHandler(req, res, next).catch(next)
